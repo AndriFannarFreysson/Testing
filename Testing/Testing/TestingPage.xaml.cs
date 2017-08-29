60eb3c36808578fd815578fc41c7acb4a66fa9c1
Andri Freysson <andriffreysson@gmail.com>
2017-08-29 00:29:26 +0000

    Adding 2 pages and making the NAvigation tab

--- Testing/Testing/TestingPage.xaml.cs
+++ Testing/Testing/TestingPage.xaml.cs
@@ -1,13 +1,29 @@
-﻿using Xamarin.Forms;
+﻿using Testing.Views;
+using Xamarin.Forms;
 
 namespace Testing
 {
-    public partial class TestingPage : ContentPage
+    public partial class TestingPage : MasterDetailPage
     {
         //Andri Fannar
         public TestingPage()
         {
             InitializeComponent();
+            Detail = new NavigationPage(new Page1());
+
+            IsPresented = false;
         }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+			IsPresented = false;
+        }
+
+		void Handle_Clicked2(object sender, System.EventArgs e)
+		{
+			Detail = new NavigationPage(new Page2());
+			IsPresented = false;
+		}
     }
 }
